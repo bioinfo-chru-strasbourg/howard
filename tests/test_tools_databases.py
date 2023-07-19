d1a8a456f5628a2cd44918b996649e601c543261
antonylebechec <antony.lebechec@gmail.com>
2023-07-19 17:11:29 +0200

    fix #49 fix tests

--- tests/test_tools_databases.py
+++ tests/test_tools_databases.py
@@ -32,6 +32,169 @@ tests_folder = os.path.dirname(__file__)
 
 
 
+def test_database():
+    """
+    This function tests the "databases" function with a set of arguments.
+    """
+
+    # Prepare arguments for the query function
+    args = argparse.Namespace(
+        assembly = 'hg19',
+        download_genomes = None,
+        download_annovar_files = None,
+        download_annovar = None,
+        download_annovar_url = None,
+        download_snpeff = None,
+        download_refseq = None,
+        config = None,
+    )
+
+    try:
+        databases(args)
+        assert True
+    except:
+        assert False
+
+
+def test_databases_download():
+    """
+    This function tests the download of databases for Annovar and snpEff tools.
+    """
+
+    # Tmp folder
+    with TemporaryDirectory(dir=".") as tmp_dir:
+
+        # assembly
+        assemblies = 'hg19'
+        assemblies_list = [value for value in assemblies.split(',')]
+
+        # Genome
+        genome_provider = None
+        genome_contig_regex = None
+
+        # tmp dir
+        genomes_tmp_dir = os.path.join(tmp_dir, "genomes")
+        os.mkdir(genomes_tmp_dir)
+        annovar_tmp_dir = os.path.join(tmp_dir, "annovar")
+        os.mkdir(annovar_tmp_dir)
+        snpeff_tmp_dir = os.path.join(tmp_dir, "snpeff")
+        os.mkdir(snpeff_tmp_dir)
+        refseq_tmp_dir = os.path.join(tmp_dir, "refseq")
+        os.mkdir(refseq_tmp_dir)
+
+        # files
+        annovar_file_list = 'nci60'
+        annovar_file_list_list = [value for value in annovar_file_list.split(',')]
+
+        # config
+        config = {"tools": {"snpeff": {"jar": DEFAULT_SNPEFF_BIN}}}
+
+        # annovar URL
+        download_annovar_url = DEFAULT_ANNOVAR_URL
+
+        # Arguments
+        args = argparse.Namespace(
+            assembly=assemblies,
+            download_genomes=genomes_tmp_dir,
+            download_genomes_provider=genome_provider,
+            download_genomes_contig_regex=genome_contig_regex,
+            download_annovar=annovar_tmp_dir,
+            download_annovar_files=annovar_file_list,
+            download_annovar_url=download_annovar_url,
+            download_snpeff=snpeff_tmp_dir,
+            download_refseq=refseq_tmp_dir,
+            download_refseq_files="ncbiRefSeq.txt,ncbiRefSeqLink.txt",
+            download_refseq_url=None,
+            download_refseq_prefix="ncbiRefSeq",
+            download_refseq_format_file="ncbiRefSeq.txt",
+            download_refseq_include_utr5=True,
+            download_refseq_include_utr3=True,
+            download_refseq_include_chrM=True,
+            download_refseq_include_non_canonical_chr=True,
+            download_refseq_include_non_coding_transcripts=True,
+            download_refseq_include_transcript_version=True,
+            config=config
+        )
+
+        # Download
+        try:
+            databases_download(args)
+            assert True
+        except:
+            assert False
+
+        # Check Genome
+        downloaded_files = os.listdir(genomes_tmp_dir)
+        for assembly in assemblies_list:
+            assert assembly in downloaded_files
+            downloaded_assembly_files = os.listdir(f"{genomes_tmp_dir}/{assembly}")
+            genome_file = f"{assembly}.fa"
+            assert genome_file in downloaded_assembly_files
+
+        # Check Annovar
+        downloaded_files = os.listdir(annovar_tmp_dir)
+        for assembly in assemblies_list:
+            for file in annovar_file_list_list:
+                downloaded_file = f"{assembly}_{file}.txt"
+                assert downloaded_file in downloaded_files
+
+        # Check snpEff databases list file
+        downloaded_files = os.listdir(snpeff_tmp_dir)
+        snpeff_databases_list_file = "snpeff_databases.list"
+        assert snpeff_databases_list_file in downloaded_files
+        # Check assembly folders of snpEff
+        for assembly in assemblies_list:
+            assert assembly in downloaded_files
+
+        # Check refSeq
+        downloaded_files = os.listdir(refseq_tmp_dir)
+        for assembly in assemblies_list:
+            assert assembly in downloaded_files
+            downloaded_refseq_files = os.listdir(f"{refseq_tmp_dir}/{assembly}")
+            refseq_file = "ncbiRefSeq.txt"
+            assert refseq_file in downloaded_refseq_files
+
+
+def test_databases_download_genomes_only():
+    """
+    This function tests the download of genomes.
+    """
+
+    # Tmp folder
+    with TemporaryDirectory(dir=".") as tmp_dir:
+
+        # assembly
+        assemblies = 'sacCer3'
+        assemblies_list = [value for value in assemblies.split(',')]
+
+        # Genome
+        genome_provider = None
+        genome_contig_regex = None
+
+        # Arguments
+        args = argparse.Namespace(
+            assembly=assemblies,
+            download_genomes=tmp_dir,
+            download_genomes_provider=genome_provider,
+            download_genomes_contig_regex=genome_contig_regex,
+            download_annovar=None,
+            download_snpeff=None,
+            download_refseq=None,
+        )
+
+        # Download
+        databases_download(args)
+
+        # Dowloaded files
+        downloaded_files = os.listdir(tmp_dir)
+        
+        # Check Genome
+        for assembly in assemblies_list:
+            assert assembly in downloaded_files
+            downloaded_assembly_files = os.listdir(f"{tmp_dir}/{assembly}")
+            genome_file = f"{assembly}.fa"
+            assert genome_file in downloaded_assembly_files
+
 
 def test_databases_format_refseq():
     """
@@ -266,30 +429,6 @@ def test_databases_download_refseq():
         assert os.path.exists(refseq_format_file_output) and os.stat(refseq_format_file_output).st_size > 0
 
 
-def test_database():
-    """
-    This function tests the "databases" function with a set of arguments.
-    """
-
-    # Prepare arguments for the query function
-    args = argparse.Namespace(
-        assembly = 'hg19',
-        download_genomes = None,
-        download_annovar_files = None,
-        download_annovar = None,
-        download_annovar_url = None,
-        download_snpeff = None,
-        config = None,
-    )
-
-    try:
-        databases(args)
-    except:
-        assert False
-
-    assert True
-
-
 def test_databases_download_genomes():
     """
     The function tests the databases_download_genomes function by checking if genomes are downloaded correctly for
@@ -460,116 +599,3 @@ def test_databases_download_annovar():
                 filtered_files = fnmatch.filter(downloaded_files, downloaded_file)
                 assert len(filtered_files) > 1
 
-
-def test_databases_download_genomes_only():
-    """
-    This function tests the download of genomes.
-    """
-
-    # Tmp folder
-    with TemporaryDirectory(dir=".") as tmp_dir:
-
-        # assembly
-        assemblies = 'sacCer3'
-        assemblies_list = [value for value in assemblies.split(',')]
-
-        # Genome
-        genome_provider = None
-        genome_contig_regex = None
-
-        # Arguments
-        args = argparse.Namespace(
-            assembly=assemblies,
-            download_genomes=tmp_dir,
-            download_genomes_provider=genome_provider,
-            download_genomes_contig_regex=genome_contig_regex,
-            download_annovar=None,
-            download_snpeff=None
-        )
-
-        # Download
-        databases_download(args)
-
-        # Dowloaded files
-        downloaded_files = os.listdir(tmp_dir)
-        
-        # Check Genome
-        for assembly in assemblies_list:
-            assert assembly in downloaded_files
-            downloaded_assembly_files = os.listdir(f"{tmp_dir}/{assembly}")
-            genome_file = f"{assembly}.fa"
-            assert genome_file in downloaded_assembly_files
-
-
-def test_databases_download():
-    """
-    This function tests the download of databases for Annovar and snpEff tools.
-    """
-
-    # Tmp folder
-    with TemporaryDirectory(dir=".") as tmp_dir:
-
-        # assembly
-        assemblies = 'hg19'
-        assemblies_list = [value for value in assemblies.split(',')]
-
-        # Genome
-        genome_provider = None
-        genome_contig_regex = None
-
-        # tmp dir
-        genomes_tmp_dir = os.path.join(tmp_dir, "genomes")
-        os.mkdir(genomes_tmp_dir)
-        annovar_tmp_dir = os.path.join(tmp_dir, "annovar")
-        os.mkdir(annovar_tmp_dir)
-        snpeff_tmp_dir = os.path.join(tmp_dir, "snpeff")
-        os.mkdir(snpeff_tmp_dir)
-
-        # files
-        annovar_file_list = 'nci60'
-        annovar_file_list_list = [value for value in annovar_file_list.split(',')]
-
-        # config
-        config = {"tools": {"snpeff": {"jar": DEFAULT_SNPEFF_BIN}}}
-
-        # annovar URL
-        download_annovar_url = DEFAULT_ANNOVAR_URL
-
-        # Arguments
-        args = argparse.Namespace(
-            assembly=assemblies,
-            download_genomes=genomes_tmp_dir,
-            download_genomes_provider=genome_provider,
-            download_genomes_contig_regex=genome_contig_regex,
-            download_annovar=annovar_tmp_dir,
-            download_annovar_files=annovar_file_list,
-            download_annovar_url=download_annovar_url,
-            download_snpeff=snpeff_tmp_dir,
-            config=config
-        )
-
-        # Download
-        databases_download(args)
-
-        # Check Genome
-        downloaded_files = os.listdir(genomes_tmp_dir)
-        for assembly in assemblies_list:
-            assert assembly in downloaded_files
-            downloaded_assembly_files = os.listdir(f"{genomes_tmp_dir}/{assembly}")
-            genome_file = f"{assembly}.fa"
-            assert genome_file in downloaded_assembly_files
-
-        # Check Annovar
-        downloaded_files = os.listdir(annovar_tmp_dir)
-        for assembly in assemblies_list:
-            for file in annovar_file_list_list:
-                downloaded_file = f"{assembly}_{file}.txt"
-                assert downloaded_file in downloaded_files
-
-        # Check snpEff databases list file
-        downloaded_files = os.listdir(snpeff_tmp_dir)
-        snpeff_databases_list_file = "snpeff_databases.list"
-        assert snpeff_databases_list_file in downloaded_files
-        # Check assembly folders of snpEff
-        for assembly in assemblies_list:
-            assert assembly in downloaded_files
