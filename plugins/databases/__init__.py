cb73ebde024fd8c3fa5ad648f7aeb89d84c93787
bioinfo <bioinfo@chru.strasbourg.fr>
2024-06-14 15:11:30 +0200

    init

---
+++ plugins/databases/__init__.py
@@ -0,0 +1,5 @@
+__description__ = "Update HOWARD databases"
+__version__ = "0.1.0"
+__enabled__ = True
+__main_file__ = "__main__"
+__main_function__ = "main"
---
+++ plugins/databases/__main__.py
@@ -0,0 +1,57 @@
+import argparse
+import logging as log
+from howard.objects.variants import Variants
+
+# Arguments
+arguments = {
+    "show": {
+        "help": """Update howard databases.\n""",
+        "action": "store_true",
+        "default": False,
+    }
+}
+
+# Command
+# commands_arguments = {
+#     "show_plugin": {
+#         "function": "show_plugin",
+#         "description": """Show variants in an input file.\n""",
+#         "help": """Short description of the plugin1""",
+#         "epilog": """Usage examples:\n"""
+#         """   howard show_plugin --input=tests/data/example.vcf.gz --output=/tmp/example.minimal.vcf.gz  --show --limit=5 \n"""
+#         """   howard show_plugin --input=tests/data/example.vcf.gz --output=/tmp/example.minimal.tsv  --show \n"""
+#         """    \n""",
+#         "groups": {
+#             "main": {"input": True, "output": False, "param": False},
+#             "Options": {"show": False, "limit": False},
+#         },
+#     }
+# }
+
+
+# Main function
+def main(args: argparse) -> None:
+    """
+    Query input VCF file and show result
+    """
+
+    # Log
+    log.info("START")
+
+    # Debug
+    log.debug(f"Input file: {args.input}")
+    log.debug(f"Output file: {args.output}")
+
+    # # Load variants file
+    # variants_obj = Variants(input=args.input, output=args.output, load=True)
+
+    # # Create query and show results
+    # query = f"SELECT * FROM variants LIMIT {args.limit}"
+    # if args.show:
+    #     df = variants_obj.get_query_to_df(query)
+    #     log.info(df)
+
+    # # Export
+    # variants_obj.export_output(query=query, export_header=True)
+
+    log.info("END")
