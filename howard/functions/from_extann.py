662661d9c24b02906465108a7f8243c87055aa8e
JbaptisteLam <jbaptiste.lamouche@gmail.com>
2024-12-17 15:34:57 +0100

    add possibility to check alias gene name in hgnc database

--- howard/functions/from_extann.py
+++ howard/functions/from_extann.py
@@ -2,17 +2,43 @@
 
 import argparse
 import logging as log
-from os.path import join as osj
 import pandas as pd
-import json
 import os
 import time
 import yaml
+import subprocess
 
 from howard.functions import commons
 
+def run_shell(command):
+    try:
+        # Run the command without check=True to handle return codes manually
+        result = subprocess.run(
+            command,
+            shell=True,
+            text=True,
+            stdout=subprocess.PIPE,  # Capture stdout
+            stderr=subprocess.PIPE,  # Capture stderr
+        )
 
-# Aim: This script was created to transform VARANK extann file to vcf, in order to be use in HOWARD 1.0.
+        if result.returncode == 0:
+            # Command succeeded and returned output
+            if result.stdout.strip():
+                return result.stdout
+            else:
+                return ""
+        elif result.returncode == 1 and "grep" in command:
+            # Handle grep's no match scenario gracefully
+            return ""
+        else:
+            # Handle other non-zero exit codes as errors
+            log.error("Error occurred!")
+            log.error(f"Return Code:, {result.returncode}")
+            log.error(f"Error Output: {result.stderr}")
+    except Exception as e:
+        log.error(f"Unexpected error: {str(e)}")
+
+
 def create_metaheader(
     df_extann: pd.DataFrame, input: str, config: dict, extra_cols=None
 ) -> str:
@@ -138,11 +164,19 @@ def write_extann(
     df_refgene,
     extra_cols=None,
     mode=None,
-    df_transcript=None,
+    df_transcript=None
 ):
     """
     Write ExtAnn into a bed like file and his hdr mate
     """
+    if os.path.exists(param.get("hgnc_file")):
+        alias = pd.read_csv(param.get("hgnc_file"), sep="\t", header=0, low_memory=False)
+        alias = alias[["symbol", "alias_symbol", "prev_symbol"]]
+        alias.fillna("NO", inplace=True)
+    else:
+        alias = None
+        log.warning("From extann will not check alias name to match extann")
+
     mandatory = ["#CHROM", "START", "END"]
     # Variants and write to file
     headerfile = output + ".hdr"
@@ -162,16 +196,20 @@ def write_extann(
         # Annotations + header
         write_header(header, o)
         df_extann.fillna(".", inplace=True)
+        log.info("Get gene coordinates from refseq")
         for i, rows in df_extann.iterrows():
-            # for each transcript
             pos_list = get_gene_coordinate(
-                df_refgene, rows, extra_cols, mode, df_transcript
+                df_refgene, rows, extra_cols, mode,df_transcript, alias
             )
             if pos_list:
+                # for each transcript from same gene
                 for data in pos_list:
+                    # it means that gene name comming from alias is already in data list
+                    if len(data) == 5:
+                        data.pop()
                     for key, val in rows.items():
                         if param.get("replace"):
-                            data.append(replace_values(str(val), param.get("replace")))
+                            data.append(replace_values(str(val),param.get("replace")))
                         else:
                             data.append(val)
                     o.write("\t".join(list(map(str, data))) + "\n")
@@ -272,6 +310,7 @@ def get_gene_coordinate(
     extra_col=None,
     mode=None,
     df_transcript=None,
+    alias=None
 ) -> any:
     """
     From pandas dataframe containing refgene file, get chr start stop from each gene present in extann
@@ -280,8 +319,10 @@ def get_gene_coordinate(
     gene_row: pandas series of extann row
     log
     """
+    gene = gene_row["genes"]
     match = df_refgene.loc[df_refgene["name"] == gene_row["genes"]]
-    if len(match.index) > 1:
+
+    if len(match.index) >= 1:
         if mode == "all":
             # Return list of list for each transcript
             return get_all_transcript(match, extra_col)
@@ -292,17 +333,103 @@ def get_gene_coordinate(
         else:
             return get_all_transcript(match, extra_col)
     else:
-        log.debug(f"Can't find {gene_row['genes']} symbol in refgene database")
+        if alias is not None:
+        #For all transcript mode search in alias file like OMIM or search in HUGO database file
+            return get_refseq_match(gene, df_refgene, alias)
 
+def get_transcript_coordinates(data, transcript):
+    """
+    """
+    data['Extracted'] = data['exon'].str.extract(r'(\d+)', expand=False).astype(int)
+    max_value = data['Extracted'].max()
+    min_value = data['Extracted'].min()
+    min_value_df = data.loc[data["Extracted"] == min_value]
+    max_value_df = data.loc[data["Extracted"] == max_value]
+    # try:
+    if data["strand"].unique() == "+":
+        yield [max_value_df["#CHROM"].iloc[0], min_value_df["START"].iloc[0], max_value_df["END"].iloc[0], transcript, max_value_df["name"].iloc[0]]
+    else:
+        yield [max_value_df["#CHROM"].iloc[0], max_value_df["START"].iloc[0], min_value_df["END"].iloc[0], transcript, max_value_df["name"].iloc[0]]
+
+def get_refseq_match(gene: str, refseq: pd.DataFrame, alias: pd.DataFrame) -> list:
+    """
+    Get from ncbi all transcript coordinate for a gene. Loop over hugo alias gene name
+
+    :param gene: gene name from gnomad gene file
+    :param refseq: path of refseq file from ncbi
+    :return: cordinate in string of each transcript with transcript name and gene name
+    """
+
+    res = refseq.loc[refseq["name"] == gene]
+    if len(res.index) == 0:
+        alias_list = get_aliases(gene, alias)
+        if not alias_list:
+            return []
+        log.info(f"Check alias gene name {gene}, list {' '.join(alias_list)}")
+        found = []
+        for snd_name in alias_list:
+            res = refseq.loc[refseq["name"] == snd_name]
+            if len(res.index) > 1:
+                for transcript, data in res.groupby("transcript"):
+                   found.extend(list(get_transcript_coordinates(data, transcript)))
+            if found:
+                log.debug(f"Found match {snd_name}")
+                return found
+        log.warning(f"{gene} not in refseq")
+        return []
+    else:
+        raise ValueError("Dataframe is not empty EXIT")
+
+def find_rows_with_substring(df: pd.DataFrame, substring:str):
+    """
+    Catch value in entire dataframe
+    """
+    mask = df.map(lambda cell: substring in str(cell))
+    if mask.any().any():
+        matching_rows = df[mask.any(axis=1)]
+        return matching_rows
+    else:
+        return ""
+
+
+def get_aliases(gene: str, alias: pd.DataFrame) -> list:
+    """
+    Get alias gene name from HGNC file
+    
+    :param gene: gene name from extann raw
+    :param alias: HNGC dataframe from raw txt file
+    """
+    try:
+        alias_gene = find_rows_with_substring(alias, gene).values.tolist()[0]
+
+    except AttributeError:
+        return []
+    alias_gene_splitted = []
+    for symbol in alias_gene:
+        if "|" in symbol:
+            symbol = symbol.split("|")
+            alias_gene_splitted.extend(symbol)
+        else:
+            alias_gene_splitted.append(symbol)
+    try:
+        alias_gene_splitted.remove(gene)
+    except ValueError:
+        log.warning(
+            f"Partial match in aliases for gene: {gene}, list {alias_gene_splitted}, skip record"
+        )
+        return []
+    
+    log.debug(alias_gene_splitted)
+    return alias_gene_splitted
 
 def from_extann(args: argparse) -> None:
     """
     This function converts an txt or tsv files containing genes-bases information\n
     From a "genes" columns which contains genes symbol it will match gene coordinates in refgene database and create a bed-like output with vcf header
 
-    :param args: `args` is an object with several attributes representing the input parameters for the
-    function. These attributes include:
-    :type args: argparse
+     :param args: `args` is an object with several attributes representing the input parameters for the
+     function. These attributes include:
+     :type args: argparse
     """
     param = read_json(args.param_extann)
     # Inputs
@@ -340,10 +467,8 @@ def from_extann(args: argparse) -> None:
 
     # Transcript
     if args.transcripts:
-        # df_transcript = pd.read_csv(args.transcript_extann, header=0, sep="\t")
         df_transcript = commons.transcripts_file_to_df(args.transcripts)
     elif param.get("transcripts"):
-        # df_transcript = pd.read_csv(param.get("transcript_extann"), header=0, sep="\t")
         df_transcript = commons.transcripts_file_to_df(param.get("transcripts"))
     else:
         if param.get("mode_extann") == "chosen":
@@ -376,7 +501,7 @@ def from_extann(args: argparse) -> None:
         df_refgene,
         param["extra_col_list"],
         args.mode_extann,
-        df_transcript,
+        df_transcript
     )
     commons.command(
         f"grep '^#' {args.output_extann}.tmp > {args.output_extann} && grep -v '^#' {args.output_extann}.tmp | sort -k1,1V -k2,2n >> {args.output_extann}"
